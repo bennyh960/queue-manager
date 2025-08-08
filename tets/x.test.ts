5bb935f6572088c07da341baf0106d681c3a7016
behassan <behassan@deloitte.co.il>
2025-08-08 10:01:39 +0300

    jest config and install

---
+++ tets/x.test.ts
@@ -0,0 +1,8 @@
+test('two plus two is four', () => {
+  const x: string = 'test';
+  const y = 4;
+  expect(x * y).toBe(4);
+});
+test('three plus three is six', () => {
+  expect(3 + 3).toBe(6);
+});
