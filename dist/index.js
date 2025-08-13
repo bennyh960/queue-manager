88260797485054ef62c5325ded082c3f7fe914bb
behassan <behassan@deloitte.co.il>
2025-08-14 00:46:01 +0300

    update

--- dist/index.js
+++ dist/index.js
@@ -3,5 +3,4 @@ export {} from './repositories/repository.interface.js';
 export { FileQueueRepository } from './repositories/file.repository.js';
 export { MemoryQueueRepository } from './repositories/memory.repository.js';
 export {} from './types/index.js';
-import * as T from './test/redis.example.js';
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -5,5 +5,3 @@ export { FileQueueRepository } from './repositories/file.repository.js';
 export { MemoryQueueRepository } from './repositories/memory.repository.js';
 
 export { type HandlerMap, type Task, type QueueManagerEvents, type QueueBackendConfig, type LoggerLike } from './types/index.js';
-
-import * as T from './test/redis.example.js';
