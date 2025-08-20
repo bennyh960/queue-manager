cedb9ffea196724590c7b4bbf17c8ea9d1d31e2d
behassan <behassan@deloitte.co.il>
2025-08-20 11:24:49 +0300

    Stop tracking dist directory

--- dist/index.d.ts
+++
@@ -1,3 +0,0 @@
-export { QueueManager } from './lib/QueueManager.js';
-export { type QueueRepository } from './repositories/repository.interface.js';
-export { type HandlerMap, type Task, type QueueManagerEvents, type QueueBackendConfig, type LoggerLike } from './types/index.js';
--- dist/index.js
+++
@@ -1,4 +0,0 @@
-export { QueueManager } from './lib/QueueManager.js';
-export {} from './repositories/repository.interface.js';
-export {} from './types/index.js';
-//# sourceMappingURL=index.js.map
